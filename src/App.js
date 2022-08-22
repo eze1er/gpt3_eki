2b603108006cd53bc2bd5e64205dbde4342d9853
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-08-21 23:41:58 -0400

    header done end responsive

--- src/App.js
+++ src/App.js
@@ -6,27 +6,25 @@ import {
   Possibility,
   Features,
   WhatGPT3,
-  Header,
+  Header
 } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-};
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
